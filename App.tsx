4286ae202eb46224f9cae40f0ecc7ee700803a8b
anabsanchez <anabsanchez.dev@gmail.com>
2024-10-19 18:24:27 +0100

    refactor: fixed indentation and variable names

--- App.tsx
+++ App.tsx
@@ -1,61 +1,110 @@
-import { useState } from 'react';
-import { Button, Pressable, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
+import { useState } from "react";
+import {
+  Button,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
+import QRCode from "react-native-qrcode-svg";
 
 export default function App() {
-  const [displayMyQR, setDisplayMyQR] = useState(true);
+  const [displayQR, setDisplayQR] = useState(true);
   return (
     <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <Text style={styles.firsttoprowContainer}>My Portfolio App</Text>
-        <View style={styles.rowTopSecondContainer}>
-        <Pressable style={styles.buttonruta} onPress={() => setDisplayMyQR(true)}>
-          <Text style={{...{color: 'white', fontWeight: 'bold', textTransform: 'uppercase'}, ...styles.shadoxboxing}}>Mi info</Text>
-        </Pressable>
-        <Button onPress={() => setDisplayMyQR(false)} title="Mi Repo" color="light-gray" accessibilityLabel='Un botón pal QR' />
+      <View style={styles.header}>
+        <Text style={styles.title}>My Portfolio App</Text>
+        <View style={styles.headerButtons}>
+          <Pressable style={styles.button} onPress={() => setDisplayQR(true)}>
+            <Text
+              style={{
+                ...{
+                  color: "white",
+                  fontWeight: "bold",
+                  textTransform: "uppercase",
+                },
+                ...styles.shadoxboxing,
+              }}
+            >
+              Mi info
+            </Text>
+          </Pressable>
+          <Button
+            onPress={() => setDisplayQR(false)}
+            title="Mi Repo"
+            color="lightgray"
+            accessibilityLabel="Un botón pal QR"
+          />
         </View>
       </View>
-      {
-        displayMyQR ?
-          <View style={styles.bodystails}>
-            <View>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Image style={styles.avatar} source={require('./assets/SofyanAmrabat.jpg')}></Image>
-              <View style={{margin: 10, backgroundColor: 'lightgray', padding: 10, borderRadius: 10, width: '70%'}}>
-            <Text style={{textAlign:'center', fontWeight: '700', fontSize: 20}}>
-            Descripción sobre mí!
-            </Text>
-            <Text>
-              Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos
-            </Text>
+      {displayQR ? (
+        <View style={styles.body}>
+          <View>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Image
+                style={styles.avatar}
+                source={require("./assets/SofyanAmrabat.jpg")}
+              ></Image>
+              <View
+                style={{
+                  margin: 10,
+                  backgroundColor: "lightgray",
+                  padding: 10,
+                  borderRadius: 10,
+                  width: "70%",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontWeight: "700",
+                    fontSize: 20,
+                  }}
+                >
+                  Descripción sobre mí!
+                </Text>
+                <Text>
+                  Soy profe y me gusta mi trabajo aunque a veces me de por
+                  enrevesar prácticas para mis queridos alumnos
+                </Text>
               </View>
-              </View>
-            <Text style= {{color: 'beriblak', fontWeight: "900", textTransform: 'capitalize', fontSize: 20, textAlign: 'center'}}>
+            </View>
+            <Text
+              style={{
+                color: "black",
+                fontWeight: "900",
+                textTransform: "capitalize",
+                fontSize: 20,
+                textAlign: "center",
+              }}
+            >
               cosas que me gustan mucho:
             </Text>
-            <ScrollView style={{padding: 10}}>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Salir a pasear</Text>
-          <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-            <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
-            <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>La guitarrita</Text>
-            <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-     <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-        <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Ir de cenar romántica</Text>
+            <ScrollView style={{ padding: 10 }}>
+              <Text style={styles.hobbies}>Salir a pasear</Text>
+              <Text style={styles.hobbies}>Senderismo</Text>
+              <Text style={styles.hobbies}>Ir a la playita</Text>
+              <Text style={styles.hobbies}>Domingos de misa</Text>
+              <Text style={styles.hobbies}>La guitarrita</Text>
+              <Text style={styles.hobbies}>El monte con lluvia</Text>
+              <Text style={styles.hobbies}>Viajar</Text>
+              <Text style={styles.hobbies}>Música variadita</Text>
+              <Text style={styles.hobbies}>Anime</Text>
+              <Text style={styles.hobbies}>Ducharme</Text>
+              <Text style={styles.hobbies}>Videojuegos</Text>
+              <Text style={styles.hobbies}>Ir de cenar romántica</Text>
             </ScrollView>
-            </View>
           </View>
-          :
-              <View style={styles.bodystails}>
-            <View style={styles.CentrarcodigoQR}>
-          <QRCode value="https://github.com/adhernea" />
-            </View>
-              </View>
-      }
+        </View>
+      ) : (
+        <View style={styles.body}>
+          <View style={styles.CentrarcodigoQR}>
+            <QRCode value="https://github.com/adhernea" />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -63,62 +112,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  topContainer: {
-    height: '15%',
+  header: {
+    height: "15%",
     paddingTop: 50,
-    width: '100%',
+    width: "100%",
   },
-  firsttoprowContainer: {
-    backgroundColor: 'gray',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    textAlignVertical: 'center',
+  title: {
+    backgroundColor: "gray",
+    textAlign: "center",
+    fontWeight: "bold",
+    textAlignVertical: "center",
     fontSize: 30,
   },
-  rowTopSecondContainer: {
-    flexDirection: 'row',
-    backgroundColor: 'darkgray',
-    justifyContent: 'center',
-    alignItems: 'center'
+  headerButtons: {
+    flexDirection: "row",
+    backgroundColor: "darkgray",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  buttonruta: {
-    width:'50%',
+  button: {
+    width: "50%",
   },
-  bodystails: {
-    width: '100%',
+  body: {
+    width: "100%",
     borderWidth: 2,
-    borderColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    height: '85%'
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: "85%",
   },
   avatar: {
     height: 90,
     width: 90,
-    borderRadius: 100
+    borderRadius: 100,
   },
-  cosasQmeGustanMuxoEstails: {
-    borderColor: 'black',
+  hobbies: {
+    borderColor: "black",
     borderWidth: 1,
-    borderStyle: 'dashed',
+    borderStyle: "dashed",
     padding: 20,
-    color: 'darkred',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontStyle: 'italic',
+    color: "darkred",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontStyle: "italic",
     fontSize: 16,
-    backgroundColor: 'silver'
+    backgroundColor: "silver",
   },
   CentrarcodigoQR: {
-    justifyContent: 'center',
+    justifyContent: "center",
     borderWidth: 1,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center'
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
   },
   shadoxboxing: {
     shadowColor: "#000",
@@ -130,5 +179,5 @@ const styles = StyleSheet.create({
     shadowRadius: 9.51,
 
     elevation: 15,
-  }
+  },
 });
